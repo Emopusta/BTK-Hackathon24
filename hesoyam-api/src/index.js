5a152d1de8c5a6c43af7efe666dfd418e9a903a7
Emopusta <emreduman225@hotmail.com>
2024-11-03 13:27:44 +0300

    Add cors to api.

--- hesoyam-api/src/index.js
+++ hesoyam-api/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors({origin: 'https://localhost:44342', credentials:true}));
+app.use(cors());
 
 app.use(serviceRegistrer)
 app.use("/api/Gemini", geminiRoutes);
