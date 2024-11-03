a89dd3c56077ba4c6aa9493a18c2f55a335fd8a8
Emopusta <emreduman225@hotmail.com>
2024-11-03 13:27:19 +0300

    Add chatting between user and bot logic.

--- hesoyam-chrome-extension/popup/popup.js
+++ hesoyam-chrome-extension/popup/popup.js
@@ -5,70 +5,103 @@ const textInputElement = document.getElementById("input-area");
 const sendButton = document.getElementById("send-button");
 const clearButton = document.getElementById("clear-button");
 
-
 chrome.storage.local.get(["chat"], (result) => {
-    const { chat } = result;
+  const { chat } = result;
+  if (chat != null) {
     currentChat = chat;
     recreateChatBoxFromList(chat);
-})
+  }
+});
 
-clearButton.onclick = function(){
-    clearChat();
-}
+clearButton.onclick = function () {
+  clearChat();
+};
 
-sendButton.onclick = function(){
-    if (!chatBoxElement) {
-        console.error("Chat box element not found.");
-        return;
-    }
-    currentChat.push({sender: 'user', message: textInputElement.value});
-    console.log(currentChat);
-    const prefs = {
-        chat: currentChat
-    }
-    chrome.runtime.sendMessage({event: 'onStart', prefs})
+sendButton.onclick = async function () {
+  if (!chatBoxElement) {
+    console.error("Chat box element not found.");
+    return;
+  }
+  currentChat.push({ sender: "user", message: textInputElement.value });
+  pushDataToChatBox({ sender: "user", message: textInputElement.value });
 
-    pushDataToChatBox({sender: 'user', message: textInputElement.value});
-    textInputElement.value = "";
-}
+  const response = await sendPrompt(textInputElement.value);
+  const botResponse = { sender: "bot", message: response.data.model.text };
+  currentChat.push(botResponse);
+  pushDataToChatBox(botResponse);
+
+  console.log(currentChat);
+  const prefs = {
+    chat: currentChat,
+  };
+  chrome.runtime.sendMessage({ event: "onStart", prefs });
+
+  textInputElement.value = "";
+};
 
 function recreateChatBoxFromList(list) {
-    list.forEach(element => {
-        const p = document.createElement("p");
-        if (element.sender == 'bot'){
-            p.classList.add('bot-message')
-        }
-        else{
-            p.classList.add('user-message')
-        }
-        p.textContent = element.message;
-        chatBoxElement.appendChild(p)
+  if (list.length > 0) {
+    list.forEach((element) => {
+      const p = document.createElement("p");
+      if (element.sender == "bot") {
+        p.classList.add("bot-message");
+      } else {
+        p.classList.add("user-message");
+      }
+      p.textContent = element.message;
+      chatBoxElement.appendChild(p);
     });
+  }
 }
 
 function pushDataToChatBox(data) {
-    const p = document.createElement("p");
-        if (data.sender == 'bot'){
-            p.classList.add('bot-message')
-        }
-        else{
-            p.classList.add('user-message')
-        }
-        p.textContent = data.message;
-        chatBoxElement.appendChild(p)
+  const p = document.createElement("p");
+  if (data.sender == "bot") {
+    p.classList.add("bot-message");
+  } else {
+    p.classList.add("user-message");
+  }
+  p.textContent = data.message;
+  chatBoxElement.appendChild(p);
 }
 
-function clearChat(){
-    currentChat = []
-    currentChat.push({sender: 'bot', message: 'Merhaba, ne aramak istiyorsunuz?'});
+function clearChat() {
+  const clearedChat = [];
+  clearedChat.push({
+    sender: "bot",
+    message: "Merhaba, ne aramak istiyorsunuz?",
+  });
 
-    while (chatBoxElement.firstChild){
-        chatBoxElement.removeChild(chatBoxElement.firstChild);
-    }
+  while (chatBoxElement.firstChild) {
+    chatBoxElement.removeChild(chatBoxElement.firstChild);
+  }
 
-    const prefs = {
-        chat: currentChat
-    }
-    chrome.runtime.sendMessage({event: 'onStart', prefs})
+  const prefs = {
+    chat: clearedChat,
+  };
+  chrome.runtime.sendMessage({ event: "onStart", prefs });
 }
 
+
+
+
+const BASE_URL = "http://localhost:8000";
+
+async function sendPrompt(promptMessage) {
+  const data = { prompt: promptMessage };
+  try {
+    const response = await fetch(BASE_URL + "/api/Gemini/send-prompt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log("error:", error);
+    return null;
+  }
+}
