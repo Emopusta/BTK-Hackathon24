a9d2712ec522a7fee610a1d0a9f8a58d1ca0f4c7
Emopusta <emreduman225@hotmail.com>
2024-11-03 23:00:21 +0300

    Refactor background unnecessary event removed.

--- hesoyam-chrome-extension/background.js
+++ hesoyam-chrome-extension/background.js
@@ -2,9 +2,6 @@
 chrome.runtime.onMessage.addListener((data) => {
 	const { event, prefs } = data;
 	switch (event) {
-		case "onStop":
-			handleOnStop();
-			break;
 		case "onStart":
 			handleOnStart(prefs);
 			break;
@@ -13,12 +10,7 @@ chrome.runtime.onMessage.addListener((data) => {
 	}
 });
 
-const handleOnStop = () => {
-	console.log("On stop in background.")
-};
-
 const handleOnStart = (prefs) => {
-	console.log("On start in background");
 	console.log("prefs received:", prefs);
     chrome.storage.local.set(prefs);
 };
